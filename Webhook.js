48238ed5af7b188317b33a20935d4d80d11ff47e
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-10-19 10:52:48 +0700

    Update Webhook.js

--- Webhook.js
+++ Webhook.js
@@ -25,29 +25,35 @@ app.use(express.urlencoded({extended:true}));
 app.post('/', (req, res) => {
         let CU  = req.body.data
         console.log(CU)
-        res.status(200).send({ message: 'Data berhasil diterima ' }); 
+        res.status(200).send({ message: 'Data berhasil diterima dan disimpan CRM' }); 
    });
 
     //////////////////////////////////////222//////////////////////////////////
     app.post('/contactus', (req, res) => {
       const source = req.body.source; 
       const status = req.body.status; 
+      const department = req.body.department;
+      const company = req.body.company;
       const name = req.body.name; 
       const email = req.body.email;
-      const department = req.body.department;
+      const address = req.body.address;
+      const description = req.body.description;
+      //%23 pengganti hastag
+      const tags = req.body.tags.split(',').map(tag => `%23${tag}`).join(',');
+      //
       const axios = require('axios');
       
       // query string 
-      const queryString = `source=${source}&status=${status}&name=${name}&email=${email}&department=${department}`;
-    
-      axios.get(`http://endpoint/contactus?${queryString}`)
+      const queryString = `source=${source}&status=${status}&department=${department}&company=${company}&name=${name}&email=${email}&address=${address}&description=${description}&tags=${tags}`;
+      axios.get(`http://server/contactus2?${queryString}`)
         .then((response) => {
+          console.log('data', queryString)
           console.log('Respon dari Web 2:', response.data);
-          res.status(200).send({ message: 'Data berhasil diterima dan disimpan pada web 2.' });
+          res.status(200).send({ message: 'Data berhasil diterima dan disimpan CRM.' });
         })
         .catch((error) => {
-          console.error('Error saat mengirim data :', error);
-          res.status(500).send({ message: 'Gagal mengirim data .' });
+          console.error('Error saat mengirim data CRM:', error);
+          res.status(500).send({ message: 'Gagal mengirim data CRM.' });
         });
     });
       
